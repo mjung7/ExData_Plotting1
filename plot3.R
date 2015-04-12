c688c2f94b09d681b41ece10eaf4add850b89565
MinJea Jung <alswp422@gmail.com>
2015-04-12 19:11:18 -0400

    png

--- plot3.R
+++ plot3.R
@@ -35,5 +35,6 @@ plot(df$DateTime,df$Sub_meeting_1,type="l", xlab="", ylab="Energy sub metering")
 lines(df$DateTime,df$Sub_meeting_2,col="red")
 lines(df$DateTime,df$Sub_meeting_3,col="blue")
 
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
 dev.off()
-legend("topright",pch=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
